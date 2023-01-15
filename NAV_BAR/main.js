432a325b5974dea8a57711bb64f54f94d145e189
codenameyujeong <salatosa1@gmail.com>
2023-01-16 01:03:21 +0900

    up

--- NAV_BAR/main.js
+++ NAV_BAR/main.js
@@ -4,6 +4,8 @@
 // window.onload = function() { 
 //오류떠서 구글링결과 script위치선정바꿈 안되서 위에 window써봄
 //그래도 안되서 그냥 id값줘서 가지고오니까 된다..
+
+
 //     console.log('안녕하세요');
 // toggleBtn.addEventListener('click', () => {
 //     menu.classList.toggle('active');
